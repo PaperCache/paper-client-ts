27ab6ae6a41222c84cbd29a0b3cf0b552e8c106a
Kia Shakiba <kiashakiba@protonmail.com>
2023-06-21 04:30:46 -0400

    Update gitignore

--- build/lib/policy.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = ;
-var Policy;
-(function (Policy) {
-    Policy[Policy["LRU"] = 0] = "LRU";
-    Policy[Policy["MRU"] = 1] = "MRU";
-})(Policy || (Policy = {}));
